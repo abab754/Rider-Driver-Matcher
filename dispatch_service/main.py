da9e5d2444027c20f9f6fc47d627ed52e07db8db
Abhiram Banda <abhiram.ban@outlook.com>
2026-09-15 15:47:43 -0400

    adding monitoring to the dispatch service and created dashboard

--- dispatch_service/main.py
+++ dispatch_service/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from dispatch_service.routes import router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
-from dispatch_service.database import Base, engine               
-import dispatch_service.models  # registers models with Base     
-                                                                
+
+from dispatch_service.database import Base, engine
+import dispatch_service.models  # registers models with Base
+
 Base.metadata.create_all(bind=engine)
 app.include_router(router)
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/health")
 def health_endpt():
